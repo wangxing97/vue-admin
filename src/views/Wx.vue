6ca0728b7bf75805313556238d028cd47faaa3ec
wangxing <1027923428@qq.com>
2020-02-10 18:45:59 +0800

    'Inititalization-successful'

---
+++ src/views/Wx.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{msg1}}</h1>
+        <h2>{{msg2}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg1:'王星一号',
+            msg2:'王星二号'
+        }
+    },
+}
+</script>
